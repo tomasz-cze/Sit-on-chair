4225eee7bf497380b27171bb42f8381fffeff5a2
Tomasz Czechlewski <tomasz.czechlewski@gmail.com>
2018-06-12 12:03:56 +0200

    first version, need to style, and add JS

---
+++ gulpfile.js
@@ -0,0 +1,21 @@
+var gulp = require("gulp");
+var sass = require("gulp-sass");
+var sourcemaps = require("gulp-sourcemaps");
+
+
+
+gulp.task("sass", function () {
+    return gulp.src("scss/main.scss")
+        .pipe (sourcemaps.init())
+        .pipe (sass({errLogToConsole: true}))
+        .on('error',function(e){
+            console.error("gulpError", e)
+        })
+        .pipe (sourcemaps.write())
+        .pipe (gulp.dest("css/."))
+});
+
+
+gulp.task("watch", function () {
+    gulp.watch("scss/**/*.scss", ["sass"]);
+});
